48658a1c02ebe94802688ce4cdb760dfa01db647
Martin Pieuchot <mpieuchot@nolizard.org>
2009-08-23 23:57:10 +0200

    Add basic login/logout views

--- settings.py
+++ settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'wombat.users',
 )
 
 AUTH_PROFILE_MODULE = 'users.Profile'
--- users/views.py
+++ users/views.py
@@ -1,1 +1,41 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.contrib import auth
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    """
+        Login method from The Django Book:
+            http://www.djangobook.com/en/2.0/chapter14/
+    """
+    if request.method == "POST":
+        username = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
+            return HttpResponseRedirect('/mail/')
+        else:
+            # TODO: Add translation string.
+            err = "The username or password you entered is incorrect."
+    else:
+        err = ''
+    return render_to_response('login.html', {'err_msg': err})
+
+
+def logout(request):
+    """
+        Return to the index after a logout, we don't care about a
+        "Thanks for your visit" page.
+    """
+    auth.logout(request)
+    return HttpResponseRedirect("/")
+
+
+@login_required
+def inbox(request):
+    return render_to_response('mail.html', {'user': request.user})

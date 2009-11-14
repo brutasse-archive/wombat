c8c5df1668ed12ea3fece15c569a5c51510cff6b
Bruno Renié <buburno@gmail.com>
2009-11-15 00:27:01 +0100

    The app shouldn't break if there is no account configured

--- mail/views.py
+++ mail/views.py
@@ -4,23 +4,27 @@ from django.contrib.auth.decorators import login_required
 
 from shortcuts import render
 from mail.models import MailForm
+from users.models import Account
 
 
 @login_required
 def inbox(request):
-    imap = request.user.get_profile().accounts.get(default=True).imap
-    inbox = imap.directories.get(name='INBOX')
-    dir_list = imap.directories.all()
+    try:
+        imap = request.user.get_profile().accounts.get(default=True).imap
+        inbox = imap.directories.get(name='INBOX')
+        dir_list = imap.directories.all()
 
-    inbox_counts = inbox.message_counts()
-    messages = inbox.message_list(number_of_messages=inbox_counts['total'])
+        inbox_counts = inbox.message_counts()
+        messages = inbox.message_list(number_of_messages=inbox_counts['total'])
 
-    context = {
-            'user': request.user,
-            'directories': dir_list,
-            'counts': inbox_counts,
-            'messages': messages,
-    }
+        context = {
+                'user': request.user,
+                'directories': dir_list,
+                'counts': inbox_counts,
+                'messages': messages,
+        }
+    except Account.DoesNotExist:
+        context = {'user': request.user}
     return render(request, 'mail.html', context)
 
 @login_required

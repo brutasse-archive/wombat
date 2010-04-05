1fd90396be542dba6a9af81e704d2a6a5806b69c
Bruno Renié <buburno@gmail.com>
2010-04-05 14:48:12 +0100

    Integrated the user profile in the admin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
-from users.models import IMAP, Directory, SMTP, Account
+from users.models import IMAP, Directory, SMTP, Account, Profile
+from django.contrib.auth.models import User
 
 class DirAdmin(admin.ModelAdmin):
     list_display = ('name', 'mailbox', 'has_children', 'no_select',
                     'unread', 'total')
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    max_num = 1
+
+
+class UserWithProfileAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline]
+
+
 admin.site.register(IMAP)
 admin.site.register(SMTP)
 admin.site.register(Account)
 admin.site.register(Directory, DirAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserWithProfileAdmin)

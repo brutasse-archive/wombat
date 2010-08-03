3b88efea65885796f6beb777ae8ab765db604a24
Bruno Renié <buburno@gmail.com>
2010-08-03 11:44:47 +0100

    Removed mail-related admin stuff from the 'users' app

--- wombat/users/admin.py
+++ wombat/users/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
-from users.models import IMAP, Mailbox, SMTP, Account, Profile
+from users.models import Account, Profile
 from django.contrib.auth.models import User
-
-class DirAdmin(admin.ModelAdmin):
-    list_display = ('name', 'imap', 'has_children', 'no_select',
-                    'unread', 'total', 'parent')
 
 
 class ProfileInline(admin.StackedInline):
@@ -17,9 +13,6 @@ class UserWithProfileAdmin(admin.ModelAdmin):
     inlines = [ProfileInline]
 
 
-admin.site.register(IMAP)
-admin.site.register(SMTP)
 admin.site.register(Account)
-admin.site.register(Mailbox, DirAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserWithProfileAdmin)
